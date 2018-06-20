946e9bb03844f41c611ef318016cc9423f70afec
kyeahen <bright19961@naver.com>
2018-06-20 23:47:26 +0900

    Create Project

---
+++ OpenAPILogin/OpenAPILogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenAPILogin
+//
+//  Created by 김예은 on 2018. 6. 20..
+//  Copyright © 2018년 yeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
